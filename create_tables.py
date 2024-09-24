a2aa0b2f9079ed361e97bd11fdf2e97499227f1e
artem.golovin <https://gitlab.kpm-ritm.ru/artem.golovin>
2024-09-24 14:33:30 +0400

    Фикс unlink файла

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,7 @@
 import sqlite3
 from pathlib import Path
 
-Path("airplanes.db").unlink()
+Path("airplanes.db").unlink(missing_ok=True)
 
 con = sqlite3.connect("airplanes.db")
 with open("sqlite_script.txt", mode="r", encoding="utf8") as tables_statements:
