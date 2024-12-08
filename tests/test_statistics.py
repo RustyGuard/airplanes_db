c44804881c379c2ae66d8895ca587293f6ea9ada
rusty <golovinartem2004@gmail.com>
2024-12-08 12:39:05 +0400

    Проверка работы страниц, которые всего лишь выводят статистику

---
+++ tests/test_statistics.py
@@ -0,0 +1,23 @@
+import pytest
+
+statistics_pages = [
+    "/get_flights_departure_times",
+    "/get_cheap_routes",
+    "/get_destination_cities",
+    "/get_routes_within_duration_interval",
+    "/get_pilots_that_start_with",
+    "/get_passengers_with_address",
+    "/get_cities_to_airports_count",
+    "/get_ticket_price_average",
+    "/get_passengers_with_payments",
+    "/get_pilots_to_flights_count",
+]
+
+
+@pytest.mark.parametrize(
+    "page_url",
+    statistics_pages,
+)
+def test_get_statistics_page(test_client, page_url: str):
+    response = test_client.get(page_url)
+    assert response.status_code == 200
